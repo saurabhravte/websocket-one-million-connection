3a16fd1852c8504d2a87495210547e679ab451d2
Saurabh Ravte <saurabh.ravte@gmail.com>
2026-08-20 20:13:26 +0530

    websocket and server setup

---
+++ server.js
@@ -0,0 +1,30 @@
+
+import http from "node:http"
+import fs from "node:fs/promises"
+import path from "node:path"
+
+import {WebSocketServer} from "ws"
+
+const PORT = process.env.PORT ?? 9000
+
+const httpServer = http.createServer(async function (req,res){
+  const indexFile = await fs.readFile(path.resolve("./index.html"), 'utf-8');
+  res.setHeader('Content-Type', 'text/html');
+  return res.end(indexFile);
+})
+
+const wsServer = new WebSocketServer({server:httpServer})
+
+wsServer.on('connection', (websocket) => {
+  console.log(`Websocket Connection...`);
+
+  websocket.on("message", (data) => {
+    console.log(`WebSocket Message Rec.`, data.toString());
+    
+  })
+})
+
+httpServer.listen(PORT,() => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+  
+})
